61abe2db1e160d9afe31f08d1a8739b573884582
Chris <chrs.bryant@gmail.com>
2019-08-20 14:09:10 -0700

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -34,29 +34,6 @@ const App = () => {
     transition: 'box-shadow 0.125s ease-out 0s',
     margin: '0px'
   }
-  
-  const inputs = [
-    {
-      data: { type: 'email', name: 'email', label: 'email', placeholder: 'enter email', required: true },
-      fieldStyle: { width: '75%', maxHeight: '5rem', justifyContent: 'space-between', flexDirection: 'column'},
-      inputStyle: { ...inputStyle }
-    },
-    {
-      data: { type: 'password', name: 'password', label: 'password', placeholder: 'enter password', required: true },
-      fieldStyle: { width: '75%', maxHeight: '5rem', justifyContent: 'space-between', flexDirection: 'column'},
-      inputStyle: { ...inputStyle }
-    }
-  ]
-
-  const buttons = [
-    { text: 'Submit', type: 'submit', cb: null, style: 'squareButton' },
-    { text: 'Cancel', type: 'cancel', cb: null, style: 'squareButton' }
-  ]
-
-  // const form = {
-  //   data: { name: 'signinForm', submit: 'signup', cb: signin },
-  //   style: { height: 1, justifyContent: 'space-around', flexDirection: 'column', px: 2, fontSizeModule: [1, 2, 3, 4]},
-  // }
 
   return (
     <Box
